fd56f060e62ff453d90d9a558e5a20073ca143c9
totalintelli <kwanwoo5@naver.com>
2015-08-10 18:18:59 +0900

    Input 함수 추가

--- CountHitAndBlow.c
+++ CountHitAndBlow.c
@@ -1,7 +1,7 @@
 //CountHitAndBlow.c
 /*
 파일 이름: CountHitAndBlow.c
-기     능: 4개의 정답수와 수를 입력받아 히트와 블로우를 구한다.
+기     능: 4개의 정답과 수를 입력받아 히트와 블로우를 구한다.
 작 성  자: 송 용 단
 작성 일자: 2015년 8월 10일
 */
@@ -25,4 +25,22 @@ int main(int argc, char *argv[]) {
 	Output(hit, blow);
 
 	return 0;
+}
+
+/*
+함수 이름: Input
+기     능: 정답과 수들을 입력받는다.
+입     력: 없음
+출     력: 정답과 수들
+*/
+void Input(ULong(*answers), ULong(*numbers)) {
+	ULong i;
+
+	for (i = 0; i < MAX; i++) {
+		scanf("%d", answers + i);
+	}
+
+	for (i = 0; i < MAX; i++) {
+		scanf("%d", numbers + i);
+	}
 }
