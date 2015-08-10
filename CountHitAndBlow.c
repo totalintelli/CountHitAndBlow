5cf1dda34dc98309acd04f31e525c75751d0b88a
totalintelli <kwanwoo5@naver.com>
2015-08-10 17:25:37 +0900

    파일이름, 기능, 작성자, 작성일자 추가

--- CountHitAndBlow.c
+++ CountHitAndBlow.c
@@ -1,4 +1,10 @@
 //CountHitAndBlow.c
+/*
+파일 이름: CountHitAndBlow.c
+기     능: 4개의 정답수와 수를 입력받아 히트와 블로우를 구한다.
+작 성  자: 송 용 단
+작성 일자: 2015년 8월 10일
+*/
 #include <stdio.h>
 #define MAX 4	
 typedef unsigned long int ULong;
