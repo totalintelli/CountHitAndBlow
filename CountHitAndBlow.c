fd3da8b1d2b2fb7bbccd2ae581084166f93ca4f4
totalintelli <kwanwoo5@naver.com>
2015-08-10 18:15:52 +0900

    main 함수 추가

--- CountHitAndBlow.c
+++ CountHitAndBlow.c
@@ -13,3 +13,16 @@ int main(int argc, char *argv[]);
 void Input(ULong (*answers), ULong (*numbers));
 void CountHitAndBlow(ULong (*answers), ULong (*numbers), ULong (*hit), ULong (*blow));
 void Output(ULong hit, ULong blow);
+
+int main(int argc, char *argv[]) {
+	ULong answers[MAX];
+	ULong numbers[MAX];
+	ULong hit;
+	ULong blow;
+
+	Input(answers, numbers);
+	CountHitAndBlow(answers, numbers, &hit, &blow);
+	Output(hit, blow);
+
+	return 0;
+}
